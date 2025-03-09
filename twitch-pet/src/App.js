9acefd49979e951864f8420a8c9b8352f66a6a42
MythicalCrewFiles <chute-awnings.0m@icloud.com>
2025-03-09 10:12:41 +0000

    Add all project files & disable Jekyll

---
+++ twitch-pet/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TwitchPet from "./TwitchPet";  // ✅ Fix: Only one `import`
+
+function App() {
+  return (
+    <div>
+      <TwitchPet />
+    </div>
+  );
+}
+
+export default App;
---
+++ twitch-pet/src/TwitchPet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TwitchPet() {
+  return <div>Twitch Pet is here!</div>;
+}
+
+export default TwitchPet;
